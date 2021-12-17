9d24f7614590e927b873e6e2f381424438d092d2
Jing Wei <jingwei@hotmail.sg>
2021-12-17 14:05:49 +0800

    Custom fetch hook

--- src/components/TripList.jsx
+++ src/components/TripList.jsx
@@ -1,30 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./TripList.css";
+import { useFetch } from "../hooks/useFetch";
 
 function TripList() {
-  const [trips, setTrips] = useState([]);
   const [url, setUrl] = useState("http://localhost:3000/trips");
-
-  useEffect(() => {
-    fetch(url) //Use the Fetch API to fetch the data from the URL stated in initial state of useState(), it can change dynamically
-      .then((response) => response.json()) //then get the response and convert it to JSON data
-      .then((json) => setTrips(json)); //then get the converted JSON data and set the trips with this data
-  }, [url]); //Setting the useState variable "url", if this url changes usEffect() will run again
-
-  console.log(trips);
+  const { data: trips } = useFetch(url); //Custom hook
 
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
       <ul>
-        {trips.map((trip) => {
-          return (
-            <li key={trip.id}>
-              <h3>Trip Title: {trip.title}</h3>
-              <p>Price: {trip.price}</p>
-            </li>
-          );
-        })}
+        {trips &&
+          trips.map((trip) => {
+            return (
+              <li key={trip.id}>
+                <h3>Trip Title: {trip.title}</h3>
+                <p>Price: {trip.price}</p>
+              </li>
+            );
+          })}
       </ul>
       <div className="filter">
         <button
